cb97a6249d78832b410f7825a36660dfc31f1597
Nikita <97429674+Astaforkin@users.noreply.github.com>
2022-11-08 11:30:00 +0300

    Update main.py
    Add class

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 from random import randint
 
+class Character:
+    # Объявляем конструктор класса.
+    def __init__(self, name):
+        self.name = name
+    
+   # Объявляем метод атаки
+    def attack(self):
+        ...
+    
+    # Объявляем метод защиты.
+    def defence(self):
+        ...
+    
+    # Объявляем метод специального умения.
+    def special(self):
+        ...
+
+class Warrior(Character):
+    ...
+
+class Mage(Character):
+    ...
+
+class Healer(Character):
+    ... 
 
 def attack(char_name, char_class):
     if char_class == 'warrior':
